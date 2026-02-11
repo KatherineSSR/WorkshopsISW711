5da04135099d71fe0dfefc720452320586ca8d4a
Katherine Solis <ktssolis36@gmail.com>
2026-02-10 20:53:54 -0600

    Model de curso modificado
    Se agregan nuevos atributos

--- Workshop03/models/course.js
+++ Workshop03/models/course.js
@@ -8,6 +8,19 @@ const courseSchema = new mongoose.Schema({
     credits: {
         required: true,
         type: Number
+    },
+    code: {
+        required: true,
+        type: Number
+    },
+    description: {
+        required: true,
+        type: String
+    },
+    teacherId: {
+        required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Teacher'
     }
 })
 
