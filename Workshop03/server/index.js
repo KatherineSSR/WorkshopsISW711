fca9ce3821a9ecfd30408ab3e4526674621a8f0e
Katherine Solis <ktssolis36@gmail.com>
2026-02-15 22:06:58 -0600

    Update- Index centraliza el enrutamiento de teacher y course
    Cambio en la arquitectura del sistema, se retira la lógica de course de index.js y ahora solo centraliza enrutamiento

--- Workshop03/server/index.js
+++ Workshop03/server/index.js
@@ -4,7 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Course = require('./models/course');
+
+// rutas de cursos y profes
+const courseRoutes = require('./routes/courseRoutes');
+const teacherRoutes = require('./routes/teacherRoutes');
 
 const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
@@ -29,79 +32,11 @@ app.use(cors({
   methods: '*'
 }));
 
+//path de node ayuda a trabajar con rutas de archivos
+const path = require('path'); 
+app.use(express.static(path.join(__dirname, '../client'))); //dirname: el directorio actual
 
-app.use(express.static('client'));
-
-
-//routes
-app.post('/course', async (req, res) => {
-    const course = new Course({
-        name: req.body.name,
-        credits: req.body.credits,
-        code: req.body.code,
-        description: req.body.description,
-        teacherId: req.body.teacherId
-    })
-
-    try {
-        const courseCreated = await course.save();
-        //add header location to the response
-        res.header('Location', `/course?id=${courseCreated._id}`);
-        res.status(201).json(courseCreated)
-    }
-    catch (error) {
-        res.status(400).json({message: error.message})
-    }
-});
-
-app.get('/course', async (req, res) => {
-    try{
-        //if id is passed as query param, return single course else return all courses
-        if(!req.query.id){
-            const data = await Course.find();
-            return res.status(200).json(data)
-        }
-        const data = await Course.findById(req.query.id);
-        res.status(200).json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
-
-app.put('/course', async (req, res) => {
-    try{
-        const course = await Course.findByIdAndUpdate(
-            req.query.id,
-            {
-                name: req.body.name,
-                credits: req.body.credits,
-                code: req.body.code,
-                description: req.body.description,
-                teacherId: req.body.teacherId
-            },
-            { new: true }
-        );
-        
-        if(!course){
-            return res.status(404).json({message: 'Curso no encontrado'})
-        }
-        
-        res.status(200).json(course)
-    }
-    catch(error){
-        res.status(400).json({message: error.message})
-    }
-})
-
-app.delete('/course', async (req, res) => {
-    try{
-        const course = await Course.findByIdAndDelete(req.query.id);
-        return res.status(204).json({message: 'Curso eliminado exitosamente'})
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+app.use('/course', courseRoutes);
+app.use('/teacher', teacherRoutes);
 
 app.listen(process.env.PORT, () => console.log(`Escuchando el puerto ${process.env.PORT}!`))
